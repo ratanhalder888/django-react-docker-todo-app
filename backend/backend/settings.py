07d51d6b956806293cd2b1f2bc784b102edcd92c
ratanhalder888 <ratanhalder888@gmail.com>
2026-05-06 21:20:39 +0600

    fix: enable dotenv for local development and update frontend dependencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv('.env.local')
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
